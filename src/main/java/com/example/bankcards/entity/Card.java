db3f6c5387cc798351ab22bba30f9aa72d16538e
Rolan <rolan.working@gmail.com>
2026-03-05 20:51:46 +0300

    first commit

---
+++ src/main/java/com/example/bankcards/entity/Card.java
@@ -0,0 +1,26 @@
+package com.example.bankcards.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Table(name = "cards")
+@Entity
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String card_number;
+    private String status;
+    private BigDecimal balance;
+    private String expire_date;
+    private String owner_name;
+}
---
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -0,0 +1,18 @@
+package com.example.bankcards.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Table(name = "users")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+}
